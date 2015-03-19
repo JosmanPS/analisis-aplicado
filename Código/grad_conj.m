bbdd62defcaf7e86e4d0d1a8eaf2ffba678cb68d
José Manuel Proudinat <JosmanPS@users.noreply.github.com>
2015-03-19 17:34:02 -0600

    Update grad_conj.m

--- Código/grad_conj.m
+++ Código/grad_conj.m
@@ -1,50 +1,60 @@
-function [x, iter] = grad_conjug(n)
+function x = grad_conj( H, g, tol1, tol2, maxit)
 
 % =========================================================
 %
-% Este programa genera una matriz aleatoria con el numero
-% de condicion indicada y el gradiente necesario para que
-% la solucion optima de la cuadratica generada por este 
-% matriz y vector tenga como solucion el vector de 1s
+% Este programa encuentra la solucion (x) del problema:
+%				Hx = g
+% a traves del metodo de gradiente conjugado. Notar que este
+% problema es equivalente al de encontrar el minimos sobre
+% una funcion cuadratica
 %
-% 12 Febrero 2015
+% 8 Marzo 2015
 %
 % Jose Manuel Proudinat Silva
 % 130056
 %
 % Entrada:
-% 	- n : indica el tamano de la matriz cuadrada n x n
-% 	- ncond : numero de condicion de la matriz
+% 	- H : matriz del problema (Hessiana)
+% 	- g : vector de respuestas (gradiente)
+% 	- tol1 : tolerancia para la norma del residuo
+% 	- tol2 : tolerancia para la norma cuadrada de 'd' 
+%			 respecto a H
+% 	- maxit : maximo numero de iteraciones
 %
-% Output:
-% 	- A  : matriz aleatoria generada con condicion 'ncond'
-% 	- g  : gradiente generado para que la solucion minima de 
-%          la cuadratica generada por A y g sea el vector 
-%          de unos
-% 	- x0 : vector aleatorio de tamano 'n' 
+% Salida:
+% 	- x : el vector solucion al problema 
 %
 % =========================================================
 
 % Calculamos los valores iniciales
-[A, g] = P1_matriz(n, 10);
+n = length(g);
 x = zeros(n,1);
-tol = 1e-5;
-
-n = size(A);
-n = n(1);
-r0 = g - A * x;
-d = r0;
+r = g - H * x;
+d = r;
 iter = 0;
 
-while(iter < n && norm(r0) > tol)
+% Guardamos los resultados para reducir operaciones
+rTr = r' * r;
+Hd = H * d;
+dHd = d' * Hd;
+norm_r = sqrt(rTr);
 
-	% Actualizamos
-	alpha = (r0' * r0) / (d' * A * d);
+while(iter < maxit && norm_r > tol1 && dHd > tol2)
+
+	% Actualizamos los valores
+	alpha = rTr / dHd;
 	x = x + alpha * d;
-	r1 = r0 - alpha * A * d;
-	Beta = (r1' * r1) / (r0' * r0);
-	d = r1 + Beta * d;
-	r0 = r1;
+	r = r - alpha * Hd;
+	Beta = (r' * r) / rTr;
+	d = r + Beta * d;
+
+	% Guardamos los resultados para reducir operaciones
+	rTr = r' * r;
+	Hd = H * d;
+	dHd = d' * Hd;
+	norm_r = sqrt(rTr);
+
+	% Siguiente iteracion
 	iter = iter + 1;
 
 end
